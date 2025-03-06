1a0d2f1ef0f28a39ea0b1bce5c3c44bbb41a2ab9
rhenzala <97076128+rhenzala@users.noreply.github.com>
2025-03-06 15:55:58 +0800

    Add controller for different routes

---
+++ controllers/appController.js
@@ -0,0 +1,100 @@
+const queries = require("../db/queries");
+require("dotenv").config();
+
+exports.getHome = async (req, res) => {
+  try{
+    const categories = await queries.getAllCategories();
+    const items = await queries.getAllItems();
+    res.render("index", { categories, items})
+  } catch(e) {
+    console.log("Error fetching data: ", e);
+    res.status(500).send("Server Error");
+  }
+};
+
+exports.getCategories = async (req, res) => {
+  try {
+    const categories = await queries.getAllCategories();
+    res.render("categories", {categories} );
+  } catch(e) {
+    console.log(e);
+    res.status(500).send("Server Error");
+  }
+};
+
+exports.getItems = async (req, res) => {
+  try {
+    const items = await queries.getAllItems();
+    res.render("items", {items} );
+  } catch(e) {
+    console.log(e);
+    res.status(500).send("Server Error");
+  }
+};
+
+exports.createCategory = async (req, res) => {
+  try {
+    const { name, description } = req.body;
+    await queries.insertCategory(name, description);
+    res.redirect("/categories");
+  } catch(e) {
+    console.log(e);
+    res.status(500).send("Server Error");
+  }
+};
+
+exports.createItem = async (req, res) => {
+  try {
+    const { name, description, price, quantity, category_id } = req.body;
+    await queries.insertItem(name, description, price, quantity, category_id);
+    res.redirect("/items");
+  } catch(e) {
+    console.log(e);
+    res.status(500).send("Server Error");
+  }
+};
+
+exports.updateItem = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, description, price, quantity, category_id } = req.body;
+    await queries.updateItem(id, name, description, price, quantity, category_id);
+    res.redirect("/items");
+  } catch (err) {
+    console.error("Error updating item:", err);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+exports.deleteCategory = async (req, res) => {
+ try{
+    const { id } = req.params;
+    const items = await queries.getItemsByCategory(id);
+    if (items.length > 0) {
+      return res.status(400).send("Cannot delete category with existing items!");
+    }
+    await queries.deleteCategory(id);
+    res.redirect("/categories");
+ } catch(e) {
+    console.log(e);
+    res.status(500).send("Server Error");
+ }
+};
+
+exports.deleteItem = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await queries.deleteItem(id);
+    res.redirect("/items");
+  } catch(e) {
+    console.log(e);
+    res.status(500).send("Server Error");
+  }
+};
+
+exports.verifyAdmin = (req, res) => {
+  if (req.body.password !== process.env.SECRET_ADMIN_PASSWORD) {
+    return res.status(403).send("Unauthorized: Incorrect admin password.");
+  }
+  res.send("Admin Verified");
+};
